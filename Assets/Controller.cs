e3ac26a41c71e504c8329c9b841a0c30027f0d65
xye <xye@csusm.edu>
2018-01-29 10:07:57 -0800

    Lecture 1 -- CharacterController

---
+++ Assets/Controller.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	public float speed;
+	public float gravity;
+	public float jumpHeight;
+	public LayerMask ground;
+	public Transform feet;
+	public AudioSource audio;
+
+	private Vector3 direction;
+	private Vector3 walkingVelocity;
+	private Vector3 fallingVelocity;
+	private CharacterController controller;
+	private float distanceToGround;
+
+	// Use this for initialization
+	void Start () {
+		speed = 5.0f;
+		gravity = 9.81f;
+		jumpHeight = 3.0f;
+		direction = Vector3.zero;
+		fallingVelocity = Vector3.zero;
+		controller = GetComponent<CharacterController>();
+		distanceToGround = 0.1f;
+		audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		direction.x = Input.GetAxis("Horizontal");
+		direction.z = Input.GetAxis("Vertical");
+		direction = direction.normalized;
+		walkingVelocity = direction * speed;
+		controller.Move(walkingVelocity * Time.deltaTime);
+		if(direction != Vector3.zero)
+			transform.forward = direction;
+
+		bool isGrounded = Physics.CheckSphere(feet.position, distanceToGround, ground, QueryTriggerInteraction.Ignore);	
+		if(isGrounded)
+			fallingVelocity.y = 0f;
+		else
+			fallingVelocity.y -= gravity * Time.deltaTime;
+		if(Input.GetButtonDown("Jump") && isGrounded) {
+			audio.Play();
+			fallingVelocity.y = Mathf.Sqrt(gravity * jumpHeight);
+		}
+		controller.Move(fallingVelocity * Time.deltaTime);
+	}
+}
